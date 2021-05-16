9c6a1971719d4d99337949d6808066e25e4bd582
rahdirs <sriduneymar11@gmail.com>
2021-05-17 04:37:47 +0530

    Added comments to Main.java (Inner Classes)

--- Abstract_InnerClass__Interface/InnerClass/src/Main.java
+++ Abstract_InnerClass__Interface/InnerClass/src/Main.java
@@ -3,7 +3,14 @@ public class Main {
         GearBox mcLaren = new GearBox(6);
         GearBox.Gear first = new GearBox.Gear(1, 12.3);
 
-        GearBox.Gear second = new GearBox.Gear(2, 16.4);
+        GearBox.Gear second = new GearBox.Gear(2, 16.4);    // for static inner class
+        /*
+        * We would use
+        * GearBox.Gear second = mcLaren.new Gear(2, 16,4);
+        * for non-static inner class!
+        *
+        * Both cases, the inner class is not local to any method!!
+        * */
 
 //        System.out.println(first.driveSpeed(1000));
     }
